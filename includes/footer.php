7faa51708b6abf9e339dbcaea92dcdbd26ec481b
Connor2308 <c0027666@hallam.shu.ac.uk>
2024-09-27 13:14:38 +0100

    template

--- includes/footer.php
+++ includes/footer.php
@@ -3,9 +3,9 @@
     <?php $year = date("y"); ?>
         <div class="panel">
             <ul>
-                <li><a href="https://www.motac.gov.my/soalan-lazim">FAQs</a></li>
-                <li><a href="https://www.motac.gov.my/dasar-privasi">Privacy Policy</a></li>
-                <li><a href="https://www.motac.gov.my/dasar-keselamatan">Security Policy</a></li>
+                <li><a href="">FAQs</a></li>
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Security Policy</a></li>
                 <li><a href="map.php">Site Map</a></li>
                 <li><a href="aboutus.php">About Us</a></li>
             </ul>
