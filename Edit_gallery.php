ef1332c38fd88204dc271928257f1a0821db11b5
Connor2308 <c0027666@hallam.shu.ac.uk>
2024-11-08 11:32:04 +0000

    clean up

--- Edit_gallery.php
+++ Edit_gallery.php
@@ -14,59 +14,14 @@
       href="css/index.css"
       media="only screen and (min-width : 720px)"
     />
-
 </head>
 <body>
     <div class="main">
         <?php include("includes/index_admin.php")
         ?>
-        <div class="content home">
+        <div class="content">
                 <h2>Gallery</h2>
                 <p>Welcome Admin to the Gallery page! You can View and edit the lists of uploaded images below</p>
-                <a href="upload.php" class="upload-images">Upload Images</a>
-                <div class="images">
-                    <?php foreach ($images as $image): ?>
-                    <?php if (file_exists($image['filepath'])): ?>
-                    <a href="#">
-                        <img src="<?=$image['filepath']?>" alt="<?=$image['description']?>" data-id="<?=$image['id']?>" data-title="<?=$image['title']?>" width="300" height="200">
-                        <span><?=$image['description']?></span>
-                    </a>
-                    <?php endif; ?>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-            <div class="image-popup"></div>
-            <script>
-            // Container we'll use to output the image
-            let image_popup = document.querySelector('.image-popup');
-            // Iterate the images and apply the onclick event to each individual image
-                document.querySelectorAll('.images a').forEach(img_link => {
-	                img_link.onclick = e => {
-		                e.preventDefault();
-		                let img_meta = img_link.querySelector('img');
-		                let img = new Image();
-		                img.onload = () => {
-			            // Create the pop out image
-			            image_popup.innerHTML = `
-				        <div class="con">
-					    <h3>${img_meta.dataset.title}</h3>
-					    <p>${img_meta.alt}</p>
-					    <img src="${img.src}" width="${img.width}" height="${img.height}">
-					    <a href="delete.php?id=${img_meta.dataset.id}" class="trash" title="Delete Image"><i class="fas fa-trash fa-xs"></i></a>
-				        </div>
-			        `;
-			        image_popup.style.display = 'flex';
-		        };
-		        img.src = img_meta.src;
-	        };
-        });
-    // Hide the image popup container, but only if the user clicks outside the image
-    image_popup.onclick = e => {
-	    if (e.target.className == 'image-popup') {
-		    image_popup.style.display = "none";
-	    }
-    };
-    </script>
     </div>
         
     <?php include("includes/footer.php")?>
