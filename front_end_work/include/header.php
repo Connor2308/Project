60c67270b21de81b828ac24dd645f7a78a7e7ed0
Nadia <142833410+nadia-sn3@users.noreply.github.com>
2024-11-13 12:34:17 +0000

    Add files via upload
    Full New Front-End + styling + sample data

---
+++ front_end_work/include/header.php
@@ -0,0 +1,28 @@
+<header>
+<div class="logo">
+<h1 class="logo-text">
+  <a href="home.php">
+    <span>Sheffield</span> Auto Parts
+  </a>
+</h1></div>
+
+<form class="search-bar">
+<input type="text" name="" placeholder="Search parts..." required>
+<button type="search-submit" aria-label="Search">
+</button>
+</form>
+
+
+<ul class="navbar">
+
+<li><a href="signin.php">Sign In</a></li>
+
+<li><a href="orders.php">View My Orders</a></li>
+
+<li><a href="adminhome.php">Welcome Username</a></li>
+
+</ul>
+
+</header>
+
+<link rel="stylesheet" href="style/header.css">
---
+++ front_end_work/suppliers.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/tablelist.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suppliers</title>
+</head>
+<body>
+
+        
+    <?php include('include/header.php')?>
+    <div class="page-container">   
+        <h2 class = "page-title">Suppliers</h2>
+
+        <div class="table-container">
+            <table class="inventory-list">
+            <thead>
+                    <tr>
+                        <th>Supplier Name</th>
+                        <th>Part Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>XYZ Motor Parts</td>
+                        <td>XYZ Part</td>
+                    </tr>
+                    <tr>
+                        <td>Tel No: 1234567890</td>
+                        <td>Address: Line 1, Line 2, Postcode</td>
+                    </tr>
+                    <tr>
+                        <td>Email:xyz@email.com</td>
+                    </tr>
+                </tbody>
+</table>
+        </div>
+
+
+    </div>
+    <?php include('include/footer.php')?>
+</body>
+</html>
