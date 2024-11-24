235e27e174338ff16ce666ccae3f9aba78d4dc37
Felix <c3032522@hallam.shu.ac.uk>
2024-11-24 23:30:37 +0000

    Xampp Breaks Again

---
+++ front_end_work/create_new_user.php
@@ -0,0 +1,24 @@
+<?php
+session_start();//https://www.php.net/manual/en/function.session-start.php, this is needed to keep my user logged in and so only they can access these pages. this is used by check login fucntion in functions 
+include("includes/connection.php");
+include("includes/functions.php");
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Creation Page</title>
+</head>
+<body>
+<?php include('include/header.php')?>
+
+
+
+<?php include('include/footer.php')?>
+</body>
+</html>
--- front_end_work/signup.php
+++ front_end_work/signup.php
@@ -8,27 +8,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
 </head>
-<body>
-
-        
+<body>  
     <?php include('include/header.php')?>
     <div class="page-container"> 
-        
-    
-    
-    <div class="form-container">
-        <form action="signup.php" method="post">
-         <h3>Sign Up</h3>
-         <input type="email" name="email" required placeholder="Email">
-         <input type="password" name="password" required placeholder="Password">
-         <input type="submit" name="submit" value="Login" class="form-btn">
-         <p>or <a href="signin.php">Sign in</a></p>
-      </form>
+        <div class="form-container">
+            <form action="signup.php" method="post">
+            <h3>Sign Up</h3>
+            <input type="email" name="email" required placeholder="Email">
+            <input type="password" name="password" required placeholder="Password">
+            <input type="submit" name="submit" value="Login" class="form-btn">
+            <p>or <a href="signin.php">Sign in</a></p>
+            </form>
+        </div>
     </div>
-
-
-    </div>
-
 <?php include('include/footer.php')?>
 </body>
 </html>
