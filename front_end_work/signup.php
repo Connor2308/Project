4447e50a82f612229837b1aeaaee87c1ac712304
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-12-03 15:09:24 +0000

    create users done(Add the table names to the document if any are different)

--- front_end_work/signup.php
+++ front_end_work/signup.php
@@ -1,3 +1,43 @@
+<?php
+
+@include 'function.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $user_type = $_POST['user_type'];
+
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $row = mysqli_fetch_array($result);
+
+      if($row['user_type'] == 'admin'){
+
+         $_SESSION['admin_name'] = $row['name'];
+         header('location:admin_page.php');
+
+      }elseif($row['user_type'] == 'user'){
+
+         $_SESSION['user_name'] = $row['name'];
+         header('location:user_page.php');
+
+      }
+     
+   }else{
+      $error[] = 'incorrect email or password!';
+   }
+
+};
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,6 +54,13 @@
         <div class="form-container">
             <form action="signup.php" method="post">
             <h3>Sign Up</h3>
+            <?php
+            if(isset($error)){
+                foreach($error as $error){
+                    echo '<span class="error-msg">'.$error.'</span>';
+                };
+            };
+            ?>
             <input type="email" name="email" required placeholder="Email">
             <input type="password" name="password" required placeholder="Password">
             <input type="submit" name="submit" value="Login" class="form-btn">
