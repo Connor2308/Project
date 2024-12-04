fd69e223f286e110189728bbafa4398fb216018a
TheCoderOwl <tjharper02@gmail.com>
2024-12-04 12:32:05 +0000

    faqs

--- faqs.php
+++ faqs.php
@@ -9,8 +9,9 @@
 <div class="main">
         <?php include("includes/index_header.php")?>
         <div class="content">
-          <h1><br><span></span></h1>
-          <p class="par"></p>
+          <h1><br><span>Commonly asked questions</span></h1>
+          <h2><br>Question</h2>
+          <p class="par"><br>Answer</p>
         </div>
     </div>
     <?php include("includes/footer.php")?>
