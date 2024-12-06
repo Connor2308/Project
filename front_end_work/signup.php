e1c3aeb9babd747e96865a6216d3dad08ba6c92e
Connor2308 <149149292+Connor2308@users.noreply.github.com>
2024-12-06 12:41:19 +0000

    forgot to change the location headers

--- front_end_work/signup.php
+++ front_end_work/signup.php
@@ -23,12 +23,12 @@ if(isset($_POST['submit'])){
       if($row['user_type'] == 'admin'){
 
          $_SESSION['admin_name'] = $row['name'];
-         header('location:admin_page.php');
+         header('location:home.php');
 
       }elseif($row['user_type'] == 'user'){
 
          $_SESSION['user_name'] = $row['name'];
-         header('location:user_page.php');
+         header('location:home.php');
 
       }
      
