a1a329491216e57e8fa44a3fd0b37de1ffa72333
Felix <c3032522@hallam.shu.ac.uk>
2024-12-11 12:50:57 +0000

    Re adding header

---
+++ front_end_work/include/header.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="style/header.css">
+<header>
+  <div class="logo">
+  <h1 class="logo-text">
+    <a href="home.php">
+      <span>Sheffield</span> Auto Parts
+    </a>
+  </h1></div>
+
+  <!-- Nav Bar -->
+  <ul class="navbar">
+    <li><a href="home.php">Welcome <?php echo htmlspecialchars($user_data['username']); ?></a></li>
+    <li><a href="logout.php">Sign Out</a></li>
+  </ul>
+
+</header>
