1a96c99733f9c8fb2ed766cdfae8cc0b790f6242
misbxh <c3009611@hallam.shu.ac.uk>
2024-12-25 22:02:13 +0000

    Header

--- front_end_work/include/header.php
+++ front_end_work/include/header.php
@@ -6,11 +6,18 @@
       <span>Sheffield</span> Auto Parts
     </a>
   </h1></div>
-
+  <!-- Search Bar -->
+  <div class="search-bar">
+    <form action="search_results.php" method="GET">
+      <input type="text" name="query" placeholder="Search..." required>
+      <button type="submit">Search</button>
+    </form>
+  </div>
   <!-- Nav Bar -->
   <ul class="navbar">
     <li><a href="home.php">Welcome <?php echo htmlspecialchars($user_data['username']); ?></a></li>
     <li><a href="logout.php">Sign Out</a></li>
   </ul>
+  
 
 </header>
