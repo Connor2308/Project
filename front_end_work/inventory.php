fc80a38729cb7cd5062a159329f16ab9236bde58
misbxh <c3009611@hallam.shu.ac.uk>
2025-01-05 22:55:25 +0000

    cleaning inventory

--- front_end_work/inventory.php
+++ front_end_work/inventory.php
@@ -105,10 +105,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['remove_part'])) {
     }
 }
 ?>
-<<<<<<< HEAD
-=======
 <!-- HTML -->
->>>>>>> 3b39427 (HTML edits)
 <!DOCTYPE html>
 <html lang="en">
 <head>
